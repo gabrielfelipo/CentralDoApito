36fc458cd6f86c9fac5e97f2ec0551bbc9763dee
Felipo <gabriel.felipo2010@gmail.com>
2023-02-23 15:17:02 -0300

    tests

---
+++ CentralDoApito/BoringDay/BoringDayTests/BoringDayTests.swift
@@ -0,0 +1,24 @@
+//
+//  BoringDayTests.swift
+//  BoringDayTests
+//
+//  Created by gabrielfelipo on 23/02/23.
+//
+
+import XCTest
+
+final class BoringDayTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(5+4, 9)
+        XCTAssertEqual(9-4, 5)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
